6b346802a7442d582dc8e4421c936d33634e5b1a
Woong Hwan Kim <samkj1@naver.com>
2022-10-21 17:04:25 +0900

    Folder Make

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,45 @@
-import { Container, CssBaseline, Grid, Typography } from "@mui/material";
+import { Container, CssBaseline, Grid, Typography } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Container maxWidth={false}>
         <Grid
           container
-          justifyContent="center"
-          alignItems="center"
-          sx={{ height: "100vh", border: "1px solid #9eff49" }}
+          justifyContent='center'
+          alignItems='center'
+          sx={{ height: '100vh', border: '1px solid #9eff49' }}
         >
-          <Grid item container maxWidth="70vw">
+          <Grid item container maxWidth='70vw'>
             <Grid
               item
               xs={6}
               sx={{
-                border: "1px solid grey",
+                border: '1px solid grey',
                 height: 600,
-                backgroundColor: "#d6fff9",
+                backgroundColor: '#d6fff9',
               }}
             >
-              <Typography variant="h2">Welcome To</Typography>
+              <Typography variant='h2'>Welcome To</Typography>
             </Grid>
             <Grid
               item
               xs={6}
               sx={{
-                border: "1px solid grey",
+                border: '1px solid grey',
                 height: 600,
-                backgroundColor: "#e481ff",
+                backgroundColor: 'secondary.main',
               }}
             >
-              <Typography variant="h2">MUI and React Setup</Typography>
+              <Typography variant='h2'>MUI and React Setup</Typography>
             </Grid>
           </Grid>
         </Grid>
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
