97638acf78d50067f9aa5026a687e79244287ddb
Woong Hwan Kim <samkj1@naver.com>
2022-10-23 14:07:02 +0900

    DrawerMenu Update

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import DrawerMenu from './components/DrawerMenu/DrawerMenu'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <DrawerMenu />
     <App />
   </React.StrictMode>
 );
